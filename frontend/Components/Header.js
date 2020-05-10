2c303fd25ad3059961be714a23ea8c51b7955b1a
Felipe Augusto Freire <44945608+felipeaugusto95@users.noreply.github.com>
2020-05-10 15:10:37 -0300

    add header component

---
+++ frontend/Components/Header.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+const Headers = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand href="/" className="text-white">Forum Bexs</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/usuarios" className="text-white">Usuários</NavLink>
+            </NavItem>
+          </Nav>
+          
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Headers;
